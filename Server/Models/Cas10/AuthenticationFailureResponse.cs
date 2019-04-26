caa3b4123bf11761c4477b15bf30afc8c8b2d25f
Blair Allen <blallen1@vt.edu>
2019-04-26 16:04:24 -0400

    Added comments and removed a useless property from the success response object

--- Server/Models/Cas10/AuthenticationFailureResponse.cs
+++ Server/Models/Cas10/AuthenticationFailureResponse.cs
@@ -1,9 +1,18 @@
 ﻿namespace Server.Models.Cas10
 {
+	/// <summary>
+	///		An object representing the CAS v1.0 response sent when authentication fails.
+	/// </summary>
 	public class AuthenticationFailureResponse
 	{
 		private const string _failureMessage = "no";
 
+		/// <summary>
+		///		Generates a string representing the CAS v1.0 response.
+		/// </summary>
+		/// <returns>
+		///		A string
+		/// </returns>
 		public override string ToString()
 		{
 			return _failureMessage + "\n\n";
--- Server/Models/Cas10/AuthenticationSuccessResponse.cs
+++ Server/Models/Cas10/AuthenticationSuccessResponse.cs
@@ -1,22 +1,33 @@
 ﻿namespace Server.Models.Cas10
 {
+	/// <summary>
+	///		An object representing the CAS v1.0 response sent when authentication succeeds.
+	/// </summary>
 	public class AuthenticationSuccessResponse
 	{
 		private const string _successMessage = "yes";
+		private string _username;
 
+		/// <summary>
+		///		Default constructor that sets the identity of the successfully authenticated user.
+		/// </summary>
+		/// <param name="username">
+		///		The identity of the authenticated user
+		/// </param>
 		public AuthenticationSuccessResponse(string username)
 		{
-			Username = username;
+			_username = username;
 		}
 
-		public string Username
-		{
-			get;
-		}
-
+		/// <summary>
+		///		Generates a string representing the CAS v1.0 response.
+		/// </summary>
+		/// <returns>
+		///		A string
+		/// </returns>
 		public override string ToString()
 		{
-			return _successMessage + "\n" + Username + "\n";
+			return _successMessage + "\n" + _username + "\n";
 		}
 	}
 }
