55c03f3b4d6b47ad38a8f45dd3774250c840eff8
Blair Allen <blallen1@vt.edu>
2019-04-26 17:17:15 -0400

    Added comments

--- Server/Models/Login/LoginModel.cs
+++ Server/Models/Login/LoginModel.cs
@@ -2,10 +2,19 @@
 {
 	using System.ComponentModel.DataAnnotations;
 
+	/// <summary>
+	///		The model used with the Login action of the CAS controller.
+	/// </summary>
 	public class LoginModel
 	{
+		/// <summary>
+		///		A message describing the error that occurred, if any.
+		/// </summary>
 		public string ErrorMessage { get; set; }
 
+		/// <summary>
+		///		A utility property that determines if an error message has been set.
+		/// </summary>
 		public bool HasError
 		{
 			get
@@ -14,6 +23,9 @@
 			}
 		}
 
+		/// <summary>
+		///		A utilility property that determines if a success message has been set.
+		/// </summary>
 		public bool HasSuccess
 		{
 			get
@@ -22,6 +34,9 @@
 			}
 		}
 
+		/// <summary>
+		///		The password entered via the login form.
+		/// </summary>
 		[Required]
 		public string Password
 		{
@@ -29,14 +44,23 @@
 			set;
 		}
 
+		/// <summary>
+		///		The service identifier specified via the request query string.
+		/// </summary>
 		public string Service
 		{
 			get;
 			set;
 		}
 
+		/// <summary>
+		///		A message describing successful form submission, if applicable.
+		/// </summary>
 		public string SuccessMessage { get; set; }
 
+		/// <summary>
+		///		The username entered via the login form.
+		/// </summary>
 		[Required]
 		public string Username
 		{
